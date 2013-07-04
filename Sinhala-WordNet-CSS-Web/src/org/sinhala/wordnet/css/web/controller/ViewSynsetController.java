4e08b2c69a5a98c4585486eb5dfe19e55d98e3c2
lakjeewa <lakjeewadmm@gmail.com>
2013-07-04 19:44:53 +0530

    Add view synset.

---
+++ Sinhala-WordNet-CSS-Web/src/org/sinhala/wordnet/css/web/controller/ViewSynsetController.java
@@ -0,0 +1,43 @@
+package org.sinhala.wordnet.css.web.controller;
+
+import net.didion.jwnl.JWNLException;
+import net.didion.jwnl.data.POS;
+import net.didion.jwnl.data.Synset;
+import net.didion.jwnl.dictionary.Dictionary;
+
+import org.sinhala.wordnet.css.jwnl.WordNetDictionary;
+import org.sinhala.wordnet.css.model.wordnet.NounSynset;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/ViewSynset")
+public class ViewSynsetController {
+
+	@RequestMapping(method = RequestMethod.GET, params = { "action=ViewSynset", "type=noun" })
+	public String viewSynset(@RequestParam(value = "id", required = false) String id, @RequestParam(value = "type", required = false) String type, ModelMap model) {
+
+		if (id != null && !"".equals(id)) {
+			Dictionary dict = WordNetDictionary.getInstance();
+			Synset synset = null;
+			try {
+				synset = dict.getSynsetAt(POS.NOUN, Long.parseLong(id));
+			} catch (NumberFormatException | JWNLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			model.addAttribute("synset", new NounSynset(synset));
+			return "ViewSynset";
+		}
+
+		else {
+			return "ViewSynset";
+		}
+
+	}
+
+}
