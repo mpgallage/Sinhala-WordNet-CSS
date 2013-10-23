39bbd246ac4d7e68c72fb0dad90350efd425d1b1
Indeewari Wijesiri <indeewarii@gmail.com>
2013-10-23 09:37:57 +0530

    Buddhika's work

--- Sinhala-WordNet-CSS-DAL/src/main/java/org/sinhala/wordnet/wordnetDB/core/SynsetMongoDbHandler.java
+++ Sinhala-WordNet-CSS-DAL/src/main/java/org/sinhala/wordnet/wordnetDB/core/SynsetMongoDbHandler.java
@@ -35,6 +35,8 @@ import org.sinhala.wordnet.wordnetDB.model.MongoSinhalaVerb;
 import org.sinhala.wordnet.wordnetDB.model.MongoSinhalaWord;
 import org.sinhala.wordnet.wordnetDB.model.MongoSinhalaWordPointer;
 
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
+
 public class SynsetMongoDbHandler {
 
 	public void addNounSynset(NounSynset nounSynset) {
@@ -382,5 +384,60 @@ public class SynsetMongoDbHandler {
 		
 		return collection;
 	}
+	public List<MongoSinhalaNoun> findNounSynsetByLemma(String word, POS pos){
+		List<MongoSinhalaSynset> collection;
+		List<MongoSinhalaNoun> nounCollection = null;
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(
+				SpringMongoConfig.class);
+		MongoOperations mongoOperation = (MongoOperations) ctx
+				.getBean("mongoTemplate");
+		Query searchSynsetQuery1 = new Query(Criteria.where("words.lemma").regex(word));
+		if(pos.equals(POS.NOUN)){
+			collection = new ArrayList<MongoSinhalaSynset>();
+			nounCollection = mongoOperation
+				.find(searchSynsetQuery1,MongoSinhalaNoun.class);
+		}
+		
+		return nounCollection;
+	}
+	
+	public List<MongoSinhalaVerb> findVerbSynsetByLemma(String word, POS pos){
+		List<MongoSinhalaSynset> collection;
+		List<MongoSinhalaVerb> verbCollection = null;
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(
+				SpringMongoConfig.class);
+		MongoOperations mongoOperation = (MongoOperations) ctx
+				.getBean("mongoTemplate");
+		Query searchSynsetQuery1 = new Query(Criteria.where("words.lemma").regex(word));
+		if(pos.equals(POS.NOUN)){
+			collection = new ArrayList<MongoSinhalaSynset>();
+			verbCollection = mongoOperation
+				.find(searchSynsetQuery1,MongoSinhalaVerb.class);
+		}
+		
+		return verbCollection;
+	}
+	
+	public List<MongoSinhalaAdjective> findAdjSynsetByLemma(String word, POS pos){
+		List<MongoSinhalaSynset> collection;
+		List<MongoSinhalaAdjective> adjCollection = null;
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(
+				SpringMongoConfig.class);
+		MongoOperations mongoOperation = (MongoOperations) ctx
+				.getBean("mongoTemplate");
+		Query searchSynsetQuery1 = new Query(Criteria.where("words.lemma").regex(word));
+		if(pos.equals(POS.NOUN)){
+			collection = new ArrayList<MongoSinhalaSynset>();
+			adjCollection = mongoOperation
+				.find(searchSynsetQuery1,MongoSinhalaAdjective.class);
+		}
+		
+		return adjCollection;
+	}
+	
+	
 	
 }
